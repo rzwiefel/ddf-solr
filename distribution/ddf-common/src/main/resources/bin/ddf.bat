ff586a82dc02b475dac0aa877c528bf55c0b6a65
Stephen Blackmore <blackmore.stephen@gmail.com>
2018-05-22 10:59:24 -0700

    DDF-0000-Added support to pull solr port from system.properties in Windows start script

--- distribution/ddf-common/src/main/resources/bin/ddf.bat
+++ distribution/ddf-common/src/main/resources/bin/ddf.bat
@@ -4,12 +4,14 @@ SETLOCAL
 SET ARGS=%*
 SET DIRNAME=%~dp0%
 SET SOLR_PID="0"
-SET SOLR_PORT=8994
-SET SOLR_VER=6.6.2
 
 PUSHD ..
 SET DDF_HOME=%CD%
 POPD
+
+for /f "tokens=2 delims==" %%G in ('findstr /i "solr.http.port=" %DDF_HOME%\etc\system.properties') do (
+    SET SOLR_PORT=%%G
+)
 
 :RESTART
 REM Remove the restart file indicator so we can detect later if restart was requested
@@ -17,8 +19,8 @@ IF EXIST "%DIRNAME%\restart.jvm" (
   DEL "%DIRNAME%\restart.jvm"
 )
 
-ECHO "Starting Solr on port %SOLR_PORT%"
-CALL %DDF_HOME%/solr-%SOLR_VER%/bin/solr.cmd start -p %SOLR_PORT%
+ECHO Starting Solr on port %SOLR_PORT%
+CALL %DDF_HOME%/solr/bin/solr.cmd start -p %SOLR_PORT%
 IF NOT ERRORLEVEL 0 (
     ECHO WARNING! Solr start process returned non-zero error code, please check solr logs
 )
@@ -35,8 +37,8 @@ IF "%1"== "daemon" (
     ECHO Restarting JVM...
     GOTO :RESTART
   ) ELSE (
-    echo "Stopping solr process on port %SOLR_PORT%"
-    CALL %DDF_HOME%/solr-%SOLR_VER%/bin/solr.cmd stop -p %SOLR_PORT%
+    echo Stopping Solr process on port %SOLR_PORT%
+    CALL %DDF_HOME%/solr/bin/solr.cmd stop -p %SOLR_PORT%
     EXIT /B %RC%
   )
 )
